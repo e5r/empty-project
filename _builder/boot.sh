c97835ffcfcc9dc1ae6bb37a8f9dc7707c80e6bf
Erlimar Silva Campos <erlimar@gmail.com>
2016-06-06 10:39:17 -0300

    Added empty Unix build scripts

---
+++ _builder/boot.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Copyright (c) E5R Development Team. All rights reserved.
+# Licensed under the Apache License, Version 2.0. More license information in LICENSE.txt.
+
+echo "[_builder/boot.sh] TODO..."
